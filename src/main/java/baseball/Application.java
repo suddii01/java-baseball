2e6f4c6fd8e7f28068939b6f44ddf800c90d7740
suddii01 <suddii01@naver.com>
2023-10-01 02:34:33 +0900

    feat: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            List<Integer> computer = getRandomNumbers();
+        }
+    }
+
+    public static List<Integer> getRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
